899d1ce585ff14fdf5daf33985fc4f700580db80
pavanlashkari <pavanlashkari27@gmail.com>
2023-09-06 18:30:00 +0530

    basic home screen and some changes

---
+++ lib/views/common_widget/image_slider.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ImagesForSlider extends StatelessWidget {
+  const ImagesForSlider({
+    Key? key,
+    required this.slideImageUrl,
+    required this.strInfo,
+    required this.backGroundColor,
+  }) : super(key: key);
+
+  final String strInfo;
+  final String slideImageUrl;
+  final Color backGroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: backGroundColor,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 216,
+            padding: const EdgeInsets.all(16),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 25,
+                  left: 10,
+                  child: SizedBox(
+                    width: 180,
+                    child: Text(
+                      strInfo,
+                      style: GoogleFonts.lato(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 19,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: FittedBox(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.deepOrangeAccent,
+                        ),
+                        child: const Text('  BOOK NOW  ',style: TextStyle(color: Colors.black)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          SizedBox(
+            width: 160.71,
+            height: 210,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              child: Image.network(
+                slideImageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
